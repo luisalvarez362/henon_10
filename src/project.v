e2ffc6f19d5487b407f1402432c4514e7d685684
luisalvarez362 <luis.alvarez362@uppuebla.edu.mx>
2026-04-29 17:57:25 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_top (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,28 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    assign uo_out[7:5]=3'b000;  
+assign uio_out = 0;
+  assign uio_oe  = 8'b11111111;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ui_in[3],ui_in[4],ui_in[5],ui_in[6],ui_in[7], 1'b0};
+//wire rst;
+   // wire valid;
+   // assign rst=!rst_n;
+
+    //henon_map #(.WIDTH(8),.FRAC(4)) henon (.clk(clk),.rst(rst),.en(ui_in[0]),.x_out(uo_out),.y_out(uio_out),.valid(valid));
+    top u1(.clk(clk),
+                 .rst_n(rst_n),
+                 .ena_henon(ui_in[0]), 
+                 .ena_ser_x(ui_in[1]),
+                 .ena_ser_y(ui_in[2]),
+                 .Q_ser_x(uo_out[0]),
+                 .eos_x(uo_out[1]),
+                 .Q_ser_y(uo_out[2]),
+                 .eos_y(uo_out[3]),
+                 .done_henon(uo_out[4])
+                );
 
 endmodule
