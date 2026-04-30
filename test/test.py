e71a7d1929ff8e94e5bfc402b5ac5fe8a811a964
luisalvarez362 <luis.alvarez362@uppuebla.edu.mx>
2026-04-30 12:06:01 -0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -34,9 +34,7 @@ async def test_project(dut):
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    print("uo_out =", dut.uo_out.value)
     assert dut.uo_out.value == 0
-    # assert dut.uo_out.value == LogicArray("0000XXXX")
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
